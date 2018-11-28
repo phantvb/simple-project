d6e201e29a3c5b67db9cbe24da66975609603869
lilijun <1836189945@qq.com>
2018-11-28 10:14:02 +0800

    账号管理开发

--- src/components/view/systemSetup/accountManage.vue
+++ src/components/view/systemSetup/accountManage.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="accountManage">
+    <el-tabs type="border-card">
 
+      <el-tab-pane label="自助直销">
+        <selfHelp></selfHelp>
+      </el-tab-pane>
+
+      <el-tab-pane label="渠道">
+
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
+  import SelfHelp from './selfHelp';
   export default {
-    name:'accountManage'
+    name:'accountManage',
+    props:[],
+    components: {SelfHelp},
+    methods:{
+    }
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   @import './accountManage.scss';
 </style>
