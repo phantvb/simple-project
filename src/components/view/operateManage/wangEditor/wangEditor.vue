6a925bbadbb0d7b4e8605e7f39329c413ec6a057
ASUA <1172786173@qq.com>
2018-12-07 15:17:51 +0800

    新增消息中心/运营管理里面的公告管理/资费及增值套餐/线路成本

---
+++ src/components/view/operateManage/wangEditor/wangEditor.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div id="editor" style="text-align:left"></div>
+  </div>
+</template>
+
+<script>
+  import E from 'wangeditor'
+
+  export default {
+    name: 'editor',
+    data () {
+      return {
+        editor: null
+      }
+    },
+    props: {
+      value: {
+        default: ''
+      }
+    },
+    mounted () {
+      var editor = new E('#editor')
+      editor.customConfig.onchange = (html) => {
+        this.formArticle.content = html
+      }
+      editor.create()
+      this.editor.txt.html(this.value) // 设置内容
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -12,109 +12,140 @@ import accountingManage from '@/components/view/accountingManage/accountingManag
 import billManage from '@/components/view/accountingManage/billManage';
 import chargeManage from '@/components/view/accountingManage/chargeManage';
 import fourBusinessManage from '@/components/view/businessAccepted/400businessManage';
+import comboSet from '@/components/view/operateManage/ComboSet';
+import noticeManage from '@/components/view/operateManage/noticeManage';
+import lineCostSetting from '@/components/view/operateManage/lineCostSetting';
+import messageCenter from '@/components/view/messageCenter/messageCenter'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path:'/',
-      redirect:'/layout',
-      hidden:true
+      path: '/',
+      redirect: '/layout',
+      hidden: true
     },
     {
       path: '/login',
       name: 'login',
       component: login,
-      hidden:true
+      hidden: true
     },
     {
-      path:'/layout',
-      name:'layout',
-      component:layout,
-      hidden:true,
-      children:[
+      path: '/layout',
+      name: 'layout',
+      component: layout,
+      hidden: true,
+      children: [
         {
-          path:'userCenter',
-          name:'用户中心',
-          component:userCenter
+          path: 'userCenter',
+          name: '用户中心',
+          component: userCenter
         },
         {
-          path:'businessInform',
-          name:'企业管理',
-          component:businessInform
+          path: 'businessInform',
+          name: '企业管理',
+          component: businessInform
         },
-      ],
-    },
-    {
-      path:'/businessInform',
-      name:'layout',
-      component:layout,
-      hidden:true,
-      children:[
         {
-          path:'businessDetail',
-          name:'企业审核详情',
-          hidden:true,
-          component:businessDetail
+          path: 'messageCenter',
+          name: '消息中心',
+          component: messageCenter
         }
       ],
     },
     {
-      path:'/accountingManage',
-      name:'账务管理',
-      component:layout,
-      children:[
+      path: '/businessInform',
+      name: 'layout',
+      component: layout,
+      hidden: true,
+      children: [
         {
-          path:'accountingManage',
-          name:'账务管理',
-          component:accountingManage
-        },
-        {
-          path:'billManage',
-          name:'账单管理',
-          component:billManage
-        },
-        {
-          path:'chargeManage',
-          name:'充值管理',
-          component:chargeManage
+          path: 'businessDetail',
+          name: '企业审核详情',
+          hidden: true,
+          component: businessDetail
         }
       ],
     },
     {
-      path:'/systemSetup',
-      name:'系统设置',
-      component:layout,
-      children:[
+      path: '/accountingManage',
+      name: '账务管理',
+      component: layout,
+      children: [
         {
-          path:'accountManage',
-          name:'账号管理',
-          component:accountManage
+          path: 'accountingManage',
+          name: '账务管理',
+          component: accountingManage
         },
         {
-          path:'systemLog',
-          name:'系统日志',
-          component:systemLog
+          path: 'billManage',
+          name: '账单管理',
+          component: billManage
         },
         {
-          path:'roleManage',
-          name:'角色管理',
-          component:roleManage
+          path: 'chargeManage',
+          name: '充值管理',
+          component: chargeManage
+        }
+      ],
+    },
+    {
+      path: '/systemSetup',
+      name: '系统设置',
+      component: layout,
+      children: [
+        {
+          path: 'accountManage',
+          name: '账号管理',
+          component: accountManage
+        },
+        {
+          path: 'systemLog',
+          name: '系统日志',
+          component: systemLog
+        },
+        {
+          path: 'roleManage',
+          name: '角色管理',
+          component: roleManage
         }
       ]
     },
     {
-      path:'/businessAccepted',
-      name:'业务受理',
-      component:layout,
-      children:[
+      path: '/businessAccepted',
+      name: '业务受理',
+      component: layout,
+      children: [
         {
-          path:'400businessManage',
-          name:'400业务管理',
-          component:fourBusinessManage
+          path: '400businessManage',
+          name: '400业务管理',
+          component: fourBusinessManage
         },
       ]
     },
+    {
+      path: '/operateManage',
+      name: '运营管理',
+      component: layout,
+      children: [
+        {
+          path: 'comboSet',
+          name: '资费及增值套餐设置',
+          component: comboSet
+        },
+        {
+          path: 'noticeManage',
+          name: '公告管理',
+          component: noticeManage
+        },
+        {
+          path: 'lineCostSetting',
+          name: '线路成本设置',
+          component: lineCostSetting
+        }
+      ]
+    }
   ]
 })
