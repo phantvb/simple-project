6afd6b3f0f010400b9c82691dbce2968d1e47152
phantvb <17858803852@163.com>
2019-01-11 17:20:24 +0800

    用户中心

--- src/components/view/userCenter/userCenter.vue
+++ src/components/view/userCenter/userCenter.vue
@@ -16,18 +16,18 @@
 				<el-button type="text" class="button" @click="editPortrait(true)">修改头像</el-button>
 			</div>
 			<div class="part message">
-				<p>登录账号 ：<span class="grey">{{form.username}}</span></p>
-				<p>用户名称：{{form.name}}</p>
-				<p>性别：{{form.sex=='man'?'男':'女'}}</p>
-				<!-- <p>身份证号：{{form.}}</p> -->
-				<p>邮箱：{{form.email}}</p>
-				<p>手机：{{form.phone}}</p>
+				<p>登录账号 ：<span class="grey">{{tableData.username}}</span></p>
+				<p>用户名称：{{tableData.name}}</p>
+				<p>性别：{{sex}}</p>
+				<p>身份证号：{{tableData.idNo}}</p>
+				<p>邮箱：{{tableData.email}}</p>
+				<p>手机：{{tableData.phone}}</p>
 			</div>
 			<div class="part message">
-				<p>角色：{{form.phone}}</p>
-				<p>地区：浙江省 杭州市 萧山区</p>
-				<p>状态：启用</p>
-				<p>备注：</p>
+				<p>角色：{{tableData.nameZh}}</p>
+				<p>地区：{{tableData.province}}{{tableData.city}}{{tableData.area}}</p>
+				<p>状态：{{tableData.enabled}}</p>
+				<p>备注：{{tableData.remark}}</p>
 			</div>
 		</section>
 		<section>
@@ -38,9 +38,9 @@
 				<el-button type="text" class="button" @click="editPass(true)">修改密码</el-button>
 			</div>
 		</section>
-		<message :show="mesEdit" @close="editMes(false)"></message>
-		<portrait :show="portraitEdit" @close="editPortrait(false)"></portrait>
-		<password :show="passEdit" @close="editPass(false)"></password>
+		<message :show="mesEdit" :data="tableData" @close="editMes(false)"></message>
+		<portrait :show="portraitEdit" :data="tableData" @close="editPortrait(false)"></portrait>
+		<password :show="passEdit" :data="tableData" @close="editPass(false)"></password>
 	</div>
 </template>
 
@@ -48,25 +48,22 @@
 	import message from './dialog/message.vue'
 	import portrait from './dialog/portrait.vue'
 	import password from './dialog/password.vue'
+
 	export default {
 		name: 'userCenter',
 		data() {
 			return {
-				username: '',
 				mesEdit: false,
 				portraitEdit: false,
 				passEdit: false,
-				form: {}
+				tableData: [],
+				sex: '', // 存放翻译之后的性别
 			}
 		},
 		components: {
 			message,
 			portrait,
 			password
-		},
-		mounted() {
-			this.username = sessionStorage.getItem("username");
-			this.fetchData();
 		},
 		methods: {
 			editMes(bol) {
@@ -77,14 +74,26 @@
 			},
 			editPass(bol) {
 				this.passEdit = bol;
-			},
-			fetchData() {
-				this.$ajax.post('/vos/user/getByUsername', { username: this.username }).then(res => {
-					if (res.code == 200) {
-						this.form = res.data;
-					}
-				})
 			}
+		},
+		created() {
+			this.$ajax.get('/vos/user/getDetail').then(res => {
+				if (res.code == 200) {
+					this.tableData = res.data;
+					if (this.tableData.sex == 'man') {
+						this.sex = '男';
+					}
+					if (this.tableData.sex == 'woman') {
+						this.sex = '女';
+					}
+					if (this.tableData.enabled == true) {
+						this.tableData.enabled = '启用';
+					}
+					if (this.tableData.enabled == false) {
+						this.tableData.enabled = '关闭';
+					}
+				}
+			});
 		}
 	}
 </script>
